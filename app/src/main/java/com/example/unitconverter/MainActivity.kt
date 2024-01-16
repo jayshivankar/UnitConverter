aea7a8d4a1da77942149192576a7a2c65845a28a
Jay <jp6631@srmist.edu.in>
2024-01-16 17:49:54 +0530

    completed unitconverter project.

---
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.unitconverter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.unitconverter.ui.theme.UnitConverterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            UnitConverterTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+@Composable
+fun UnitConverter() {
+    Column {
+        Text(text = "UnitConverter")
+        OutlinedTextField(
+            value = "",
+            onValueChange = {//here goes what should happen , when the value of our outlinedtextfield changes
+            })
+        Row {
+            // here all the UI elements will be stacked next to each other
+        }
+
+    }
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        UnitConverterTheme {
+            Greeting("Android")
+        }
+    }
+}
